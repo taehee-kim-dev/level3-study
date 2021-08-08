717df995e8cfe228ed880df8083c0e699ca68d61
KIM TAE HEE <taehee.kim.dev@gmail.com>
2021-08-09 05:49:39 +0900

    feat: active test profiles

--- src/test/java/my/level3study/domain/MemberRepositoryTest.java
+++ src/test/java/my/level3study/domain/MemberRepositoryTest.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ActiveProfiles("test")
 @DisplayName("MemberRepository 테스트")
 @DataJpaTest
 class MemberRepositoryTest {
