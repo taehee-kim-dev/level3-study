619bd05669f14401025a3b7000a0d01d80030ef5
KIM TAE HEE <taehee.kim.dev@gmail.com>
2021-08-09 05:58:22 +0900

    feat: deploy profile controller 추가 및 테스트

---
+++ src/main/java/my/level3study/deploy/ProfileController.java
@@ -0,0 +1,26 @@
+package my.level3study.deploy;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class ProfileController {
+
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String findProfile() {
+        List<String> deployProfiles = Arrays.asList("deploy1", "deploy2");
+
+        return Arrays.stream(env.getActiveProfiles())
+                .filter(deployProfiles::contains)
+                .findAny()
+                .orElseThrow(() -> new IllegalStateException("현재 실행중인 JAR 파일의 profile중에 deploy1와 deploy2가 모두 없습니다."));
+    }
+}
